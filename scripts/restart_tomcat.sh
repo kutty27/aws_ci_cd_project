07575a3ead84cbfd21502d7678617cc262e021c9
kutty27 <154492609+kutty27@users.noreply.github.com>
2025-03-28 11:37:18 +0530

    Update restart_tomcat.sh

--- scripts/restart_tomcat.sh
+++ scripts/restart_tomcat.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Stop Tomcat
 /opt/tomcat/apache-tomcat-9.0.75/bin/shutdown.sh
 sleep 5
+
+# Remove the existing WAR file and exploded directory
+rm -rf /opt/tomcat/apache-tomcat-9.0.75/webapps/ProductManipulation.war
+rm -rf /opt/tomcat/apache-tomcat-9.0.75/webapps/ProductManipulation/
+
+# Start Tomcat
 /opt/tomcat/apache-tomcat-9.0.75/bin/startup.sh
