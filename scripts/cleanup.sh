158c54aff19d94f9b62fafda5e6f7562db2076c3
kutty27 <154492609+kutty27@users.noreply.github.com>
2025-03-28 11:37:48 +0530

    Create cleanup.sh

---
+++ scripts/cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Remove the existing WAR file and extracted directory before copying the new one
+rm -rf /opt/tomcat/apache-tomcat-9.0.75/webapps/ProductManipulation.war
+rm -rf /opt/tomcat/apache-tomcat-9.0.75/webapps/ProductManipulation/
